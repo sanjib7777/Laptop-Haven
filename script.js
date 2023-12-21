b47e8b5849f417577b7808f0da68a58c16180c3a
sanjib7777 <sanjibshah007@gmail.com>
2023-12-21 08:34:30 +0545

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.add-btn').addEventListener('click',() =>{
+    document.querySelector('.give_detail').style.display="block";
+})
+document.querySelector('.fa-circle-xmark').addEventListener('click',() =>{
+    document.querySelector('.give_detail').style.display="none";
+})
